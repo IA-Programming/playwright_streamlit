07accd2fa491a3df6f6bc86a4c5ef84fbf806f4b
IA-Programming <sm4rt.j.r@gmail.com>
2024-05-01 00:51:35 -0400

    to deploy

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 Streamlit project to try to get playwright running on streamlit cloud.
 
+# Web scraping on Streamlit Cloud with Playwright
+
+[![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://playwright.streamlit.app/)
+
+This is a minimal, reproducible example of how to scrape the web with playwright and Chrome on Streamlit's Community Cloud. 
+
+Fork this repo, and edit `streamlit_app.py` to customize this app to your heart's desire. ❤️
+
 ## Status
 
 Just an idea, not working yet.
@@ -13,3 +21,15 @@ Just an idea, not working yet.
 - Make a working example
 - Update the whole repo
 - Test it on streamlit cloud
+
+## Resources
+
+- https://github.com/Franky1/Streamlit-Playwright?tab=readme-ov-file
+
+## Suppport Us
+
+<a href="https://www.buymeacoffee.com/blazzmocompany"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=blazzmocompany&button_colour=40DCA5&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00"></a>
+
+
+[!["Buy Me A Coffee"](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://www.buymeacoffee.com/blazzmocompany)
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,76 @@
+import requests
+from time import time
+import streamlit as st
+import asyncio
+from playwright.async_api import async_playwright
 
+"""
+\## Web scraping on Streamlit Cloud with Playwright
+
+\[!\[Source\](https://img.shields.io/badge/View-Source-<COLOR>.svg)\](https://github.com/your-repo/streamlit-playwright/)
+
+This is a minimal, reproducible example of how to scrape the web with Playwright on Streamlit's Community Cloud.
+
+Fork this repo, and edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
+"""
+
+with st.echo():
+
+    @st.cache_resource
+    def get_browser():
+        playwright = asyncio.get_event_loop().run_until_complete(async_playwright().start())
+        browser = asyncio.get_event_loop().run_until_complete(playwright.chromium.launch())
+        return browser
+
+    browser = get_browser()
+
+    @st.cache_data
+    async def ExtrayendoHTML(Url: str, browser=browser):
+        response = requests.get(Url)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            result = 'Using BeautifulSoup'
+            print('\033[1;32m' + result + '\033[0m')
+            return result, response.text
+
+        else:
+            result = 'Using Playwright'
+            print('\033[1;33m' + result + '\033[0m')
+
+            page = await browser.new_page()
+
+            # Load the webpage
+            await page.goto(Url)
+
+            # Wait for the page to fully load
+            await page.wait_for_load_state("domcontentloaded")
+
+            # Get the HTML content directly from the browser's DOM
+            html_code = await page.content()
+
+            # Get the status code using requests library
+            response = requests.get(page.url)
+
+            # Validate the status code
+            if response.status_code == 200:
+                result = result + ": " + "HTML code extracted successfully"
+                print(result)
+                await page.close()
+                return result, html_code
+            else:
+                result = result + ": " + "Failed to extract HTML code"
+                print(result)
+                await page.close()
+                return result, html_code
+
+    async def main():
+        if url := st.text_input(label="put the url that you want you extract the html code", value="http://example.com", max_chars=100, help="test"):
+            start_time = time()
+            result, html = await ExtrayendoHTML(Url=url)
+            st.markdown(result)
+            st.code(html)
+            elapsed_time = time() - start_time
+            st.markdown(f"### for the extraction of the html code is loaded in {elapsed_time:.2f} seconds")
+
+    asyncio.run(main())
